fdb777943c5e432b920a4a967d41705029e98144
Daan Rosendal <daanrosendal18@outlook.com>
2020-02-03 22:43:47 +0100

    Responsiveness CRUD projecten tabel voor admins verbeterd

--- resources/views/admin/projecten/index.blade.php
+++ resources/views/admin/projecten/index.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-8 text-center mb-4">
                 <a class="btn btn-primary btn-lg" href="{{ route('admin.projecten.create') }}" role="button">Project Aanmaken</a>
             </div>
-            <div class="col-md-8">
+            <div class="col-lg-8 col-12">
                 @if(session('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         {{session('success')}}
@@ -18,7 +18,7 @@
                 <div class="card">
                     <div class="card-header">Projecten</div>
                     <div class="card-body">
-                        <div class="table-responsive-xl">
+                        <div class="table-responsive">
                             <table class="table table-striped table-bordered mb-0">
                                 <thead>
                                     <tr>
@@ -26,7 +26,8 @@
                                         <th scope="col">Naam</th>
                                         <th scope="col">Aangemaakt</th>
                                         <th scope="col">Aangepast</th>
-                                        <th scope="col" style="width: 16%">Acties</th>
+                                        <th scope="col"><i class="fas fa-edit fa-lg"></i></th>
+                                        <th scope="col"><i class="fas fa-trash-alt fa-lg"></i></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -38,10 +39,11 @@
                                             <td>{{ \Carbon\Carbon::parse($project->updated_at)->format('j-n-Y')}}</td>
                                             <td>
                                                 <a href="{{ route('admin.projecten.edit', $project) }}"><i class="far fa-edit fa-lg"></i></a>
-                                                <a href="{{ route('admin.projecten.delete', $project) }}"
-                                                   onclick="return confirm('Weet je zeker dat je dit project wilt verwijderen? Je kunt dit niet ongedaan maken.')">
-                                                    <i class="far fa-trash-alt fa-lg ml-2"></i></a>
+
                                             </td>
+                                            <td><a href="{{ route('admin.projecten.delete', $project) }}"
+                                                   onclick="return confirm('Weet je zeker dat je dit project wilt verwijderen? Je kunt dit niet ongedaan maken.')">
+                                                    <i class="far fa-trash-alt fa-lg"></i></a></td>
                                         </tr>
                                     @endforeach
                                 </tbody>
